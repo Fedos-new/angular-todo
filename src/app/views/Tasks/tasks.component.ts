b659cb5f22d0a505087542981515de628525147e
Fedos-new <fedluky@gmail.com>
2021-02-10 20:43:09 +0700

    added sort tasks and pagination

--- src/app/views/Tasks/tasks.component.ts
+++ src/app/views/Tasks/tasks.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 import {DataHandlerService} from '../../service/data-handler.service';
 import {Task} from '../../model/Task';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 
 @Component({
@@ -9,16 +11,21 @@ import {MatTableDataSource} from '@angular/material/table';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
   // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
-   displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
-   dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+  // ссылки на компоненты таблицы
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
 
 
   tasks: Task[];
 
-  constructor(private dataHandler: DataHandlerService) { }
+  constructor(private dataHandler: DataHandlerService) {
+  }
 
   ngOnInit(): void {
     this.dataHandler.taskSubject.subscribe(task => this.tasks = task);
@@ -27,6 +34,10 @@ export class TasksComponent implements OnInit {
     this.dataSource = new MatTableDataSource();
 
     this.refreshTable();
+  }
+
+  ngAfterViewInit(): void {
+    this.addTableObjects();
   }
 
   toggleClassCompleted(task: Task): void {
@@ -53,7 +64,29 @@ export class TasksComponent implements OnInit {
   private refreshTable(): void {
 
     this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
-
-
+    this.addTableObjects();
+    // @ts-ignore
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      switch (colName) {
+        case 'priority' : {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category' : {
+          return task.category ? task.category.title : null;
+        }
+        case 'date' : {
+          return task.date ? task.date : null;
+        }
+        case 'title' : {
+          return task.title;
+        }
+      }
+    };
   }
+
+  private addTableObjects(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
