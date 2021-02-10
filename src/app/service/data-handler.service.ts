9b57f0dbded10e72f1793cbd98a1342145b77ffb
Fedos-new <fedluky@gmail.com>
2021-02-10 10:56:20 +0700

    show all tasks initially

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,24 +1,25 @@
-import { Injectable } from '@angular/core';
-import { TestData } from '../data/TestData';
+import {Injectable} from '@angular/core';
+import {TestData} from '../data/TestData';
 import {Category} from '../model/Category';
 import {Task} from '../model/Task';
-import {Subject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
-  taskSubject = new Subject<Task[]>();
+  taskSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
 
   constructor() {
+    this.fillTasks();
   }
-  getCategories(): Category[] {
-    return TestData.categories;
-  }
+
   fillTasks(): void {
     this.taskSubject.next(TestData.tasks);
   }
+
   fillTasksByCategory(category: Category): void {
     const tasks = TestData.tasks.filter(t => t.category === category);
     this.taskSubject.next(tasks);
